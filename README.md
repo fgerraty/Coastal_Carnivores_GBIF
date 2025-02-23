1c0c17a3b3cffe1996bda2f3025e3568235dd5fe
Frankie Gerraty <frankiegerraty@gmail.com>
2025-02-22 21:13:53 -0800

    getting started

---
+++ README.md
@@ -0,0 +1,7 @@
+## **Coastal Carnivores GBIF** 
+
+**Data**
+
+Subfolder "shapefile," which houses the GSHHG global shoreline shapefile. Note that the shapefile is not accessible on github due to the large file size. To reproduce spatial analyses that leverage the shapefile, download the GSHHG shapefile (v.2.3.7) from one of the following links, and place the shapefile within a subfolder called "shapefile" in the "data" folder:
+
+[GSHHG Database Website](https://www.soest.hawaii.edu/pwessel/gshhg/) or [NOAA NCEI Data Portal](https://www.ngdc.noaa.gov/mgg/shorelines/)
---
+++ scripts/00_Packages.R
@@ -0,0 +1,13 @@
+##########################################################################
+# Coastal Carnivores Project #############################################
+# Author: Frankie Gerraty (frankiegerraty@gmail.com; fgerraty@ucsc.edu) ##
+##########################################################################
+# Script 00: Load packages ###############################################
+#-------------------------------------------------------------------------
+
+# Part 1: Load Packages --------------------------------------------------
+
+# Load packages
+packages <- c("tidyverse", "sf")
+
+pacman::p_load(packages, character.only = TRUE); rm(packages)
